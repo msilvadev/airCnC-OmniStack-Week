3ce43c2bb85c37da8180a4f8a97904d5a2ef208d
Matheus Silva <msilva.jus@gmail.com>
2019-10-02 22:51:30 -0300

    Ajustes no backend Criado todo o front em ReactJS.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -13,6 +16,9 @@ mongoose.connect('mongodb+srv://msilva:msilva@omnistack-19acx.mongodb.net/semana
 // req.params = Acessar routes params (para edição, delete)
 // req.body = Acessar o corpo da requisição (criação, edição)
 
+//app.use(cors({ origin: 'http://localhost:3333' }));
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 app.listen(3333);
