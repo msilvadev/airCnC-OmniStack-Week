0029377fef634dfe9e93a5354ec29a3c1fb7ad06
Matheus Silva <msilva.jus@gmail.com>
2019-10-06 17:28:31 -0300

    ajuste

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'https://aircnc-backend-node.herokuapp.com',
 });
 
 export default api;
