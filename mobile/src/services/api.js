7179b0c812b846960106a48c7e4b4cc360019968
Matheus Silva <msilva.jus@gmail.com>
2019-10-06 02:02:31 -0300

    Mobile finalizado

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.56.1:3333',
+    baseURL: 'https://aircnc-backend-node.herokuapp.com',
 });
 
 export default api;
